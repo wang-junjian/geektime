caa6e681aec829287f5605dbdf8ceaeee2116c0b
wang-junjian <wang-junjian@qq.com>
2019-04-07 01:13:46 +0800

    sync.WaitGroup

--- Go语言从入门到实战/go_learning/src/ch16/share_memory/share_memory_test.go
+++ Go语言从入门到实战/go_learning/src/ch16/share_memory/share_memory_test.go
@@ -41,3 +41,25 @@ func TestCounterThreadSafe(t *testing.T)  {
   time.Sleep(time.Microsecond * 50)
   fmt.Println("counter = ", counter)
 }
+
+func TestCounterWaitGroup(t *testing.T)  {
+  var mutex sync.Mutex
+  var waitGroup sync.WaitGroup
+
+  counter := 0
+  for i := 0; i < 5000; i++ {
+    waitGroup.Add(1)
+    go func() {
+      defer func ()  {
+        mutex.Unlock()
+        waitGroup.Done()
+      }()
+
+      mutex.Lock()
+      counter++
+    }()
+  }
+  waitGroup.Wait()
+  
+  fmt.Println("counter = ", counter)
+}
