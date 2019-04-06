6b45fe4f0eaa291e5d1e3d55e46cb1e478d537b6
wang-junjian <wang-junjian@qq.com>
2019-04-07 00:45:43 +0800

    协程　goroutine

---
+++ Go语言从入门到实战/go_learning/src/ch16/goroutine/goroutine_test.go
@@ -0,0 +1,17 @@
+package groutine_test
+
+import (
+  "testing"
+  "fmt"
+  "time"
+)
+
+func Test(t *testing.T)  {
+  for i := 0; i < 10; i++ {
+    go func (i int)  {
+      fmt.Println(i)
+    }(i)
+  }
+
+  time.Sleep(time.Millisecond * 50)
+}
